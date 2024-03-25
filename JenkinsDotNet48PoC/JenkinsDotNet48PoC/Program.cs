8f22a9e349a6c469b3d2edbfb43df3efd002f5ad
Ray Bishun <ray.bishun@bitsbytes.com>
2024-03-24 22:15:35 -0400

    Init Commit

---
+++ JenkinsDotNet48PoC/JenkinsDotNet48PoC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDotNet48PoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! FW 4.8");
+        }
+    }
+}
